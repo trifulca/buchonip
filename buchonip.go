dbbbad8750e47bc1104df890273f7c588a3a1b3b
Ignacio Juan Martín Benedetti <tranceway@gmail.com>
2018-11-01 00:53:17 -0300

    #1 soporte para txt y json

--- buchonip.go
+++ buchonip.go
@@ -8,11 +8,37 @@ import (
 	"strings"
 )
 
-func requestHandler(res http.ResponseWriter, req *http.Request) {
-	res.Header().Set("Server", "BuchonIP/0.1")
+func parse_ip(remoteAddr string) string {
+	semicolonIndex := strings.LastIndex(remoteAddr, ":")
+	return remoteAddr[:semicolonIndex]
+}
 
-	semicolon_index := strings.LastIndex(req.RemoteAddr, ":")
-	remote_ip := req.RemoteAddr[:semicolon_index]
+func homeHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Server", "BuchonIP/0.1")
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	remote_ip := parse_ip(req.RemoteAddr)
+
+	log.Printf("Incoming request from %s", remote_ip)
+	fmt.Fprintf(res, `<p>Su IP es <b>%s</b></p>
+Disponible en <a href="/txt">txt</a> y <a href="/json">json</a>`, remote_ip)
+}
+
+func jsonHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Server", "BuchonIP/0.1")
+	res.Header().Set("Content-Type", "application/json")
+
+	remote_ip := parse_ip(req.RemoteAddr)
+
+	log.Printf("Incoming request from %s", remote_ip)
+	fmt.Fprintf(res, "{\"ip\": \"%s\"}", remote_ip)
+}
+
+func txtHandler(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Server", "BuchonIP/0.1")
+	res.Header().Set("Content-Type", "text/plain")
+
+	remote_ip := parse_ip(req.RemoteAddr)
 
 	log.Printf("Incoming request from %s", remote_ip)
 	fmt.Fprintf(res, remote_ip)
@@ -26,6 +52,8 @@ func main() {
 	address := fmt.Sprintf("%s:%d", *host, *port)
 	log.Printf("Listening at http://%s\n", address)
 
-	http.HandleFunc("/", requestHandler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/json", jsonHandler)
+	http.HandleFunc("/txt", txtHandler)
 	log.Fatal(http.ListenAndServe(address, nil))
 }
